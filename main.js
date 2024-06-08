c53bd1d82a61c8135ee63f9a6c33efaa5393c094
Daniel Vivas <danieldinosaurio77@gmail.com>
2024-06-08 01:31:17 -0600

    Consulta a la trivia

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 import { showCategories } from "./scripts/consultaCategorias.js";
+import { valor } from "./scripts/consultaPreguntas.js";
 
 showCategories()
+
+document.querySelector("#start").addEventListener("click", (e) =>{
+    e.preventDefault()
+    valor()
+
+    let categoria = valor.categoria
+    let dificultad = valor.dificultad
+    let tipoPregunta = valor.tipoPregunta
+
+    async function opciones() {
+        try {
+            const response = await fetch(`https://opentdb.com/api.php?amount=10&category=${categoria}&difficulty=${dificultad}&type=${tipoPregunta}`)
+            const data = await response.json()
+            console.log(data)
+            
+        } catch (error) {
+
+        }
+    }
+    opciones()
+})
 
