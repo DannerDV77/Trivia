33575d5604f2aa93ab269ac35c7a718e3f94787f
Daniel Vivas <danieldinosaurio77@gmail.com>
2024-06-11 03:56:39 -0600

    Preguntas multiples terminado 100%

--- scripts/preguntas.js
+++ scripts/preguntas.js
@@ -2,72 +2,133 @@ import { questions } from "../main.js";
 
 const pregunta = document.querySelector("#datoPregunta")
 const contAnsowers = document.querySelector("#contAnsowers")
+const modalGood = document.querySelector("#modal_correct_answer")
+const modalBad = document.querySelector("#modal_incorrect_answer")
+const modalFinal = document.querySelector("#modal_final")
+const contResult = document.querySelector("#contResult")
 
 let aciertos = 0
 let contador = 0
 
-
-
 export function imprimir() {
 
-    let question = questions[contador].question
-        .replace(/&quot;/g, '\"')
-        .replace(/&lt;/g, '<')
-        .replace(/&gt;/g, '>')
+    if (contador === 10) {
+        console.log(`fin de la trivia. puntuacion: ${aciertos}`)
+        const reultado = document.createElement("p")
+        reultado.innerHTML = `Puntaje: ${aciertos}`
+        contResult.appendChild(reultado)
+        modalFinal.showModal()
+        document.querySelector("#finalTrivia").addEventListener("click", () => {
+            window.location.reload()
+        })
+    } else {
+        let question = questions[contador].question
+            .replace(/&quot;/g, '\"')
+            .replace(/&lt;/g, '<')
+            .replace(/&gt;/g, '>')
 
-    let correct_answer = questions[contador].correct_answer
-    let incorrect_answers = questions[contador].incorrect_answers
-    let tipoPregunta = questions[0].type
+        let correct_answer = questions[contador].correct_answer
+            .replace(/&quot;/g, '\"')
+            .replace(/&lt;/g, '<')
+            .replace(/&gt;/g, '>')
+        let incorrect_answers = questions[contador].incorrect_answers
 
-    pregunta.innerHTML = `${question}`
+        let tipoPregunta = questions[0].type
 
-    if (tipoPregunta == "multiple") {
-        const contendorBtn = document.createElement("div")
-        contendorBtn.id = "respuestas"
-        contendorBtn.innerHTML = `
+        pregunta.innerHTML = `Question: ${question}`
+
+        if (tipoPregunta == "multiple") {
+            const contendorBtn = document.createElement("div")
+            contendorBtn.id = "respuestas"
+            contendorBtn.innerHTML = `
         <button id="btn1"></button>
         <button id="btn2"></button>
         <button id="btn3"></button>
         <button id="btn4"></button>`
 
-        contAnsowers.appendChild(contendorBtn)
+            contAnsowers.appendChild(contendorBtn)
 
-        const respuestas = [
-            correct_answer,
-            ...incorrect_answers
-        ].sort(() => Math.random() - 0.5)
+            const respuestas = [
+                correct_answer,
+                ...incorrect_answers
+            ].sort(() => Math.random() - 0.5)
 
-        const botones = contendorBtn.querySelectorAll("button")
-        botones.forEach((button, Index) => {
-            button.textContent = respuestas[Index]
-        })
+            let indiceRespuestaCorrecta;
 
-        console.log(respuestas, botones, correct_answer);
-        console.log(correct_answer);
+            
+            indiceRespuestaCorrecta = respuestas.findIndex(respuesta => respuesta === correct_answer);
 
-        const btn1 = document.querySelector("#btn1")
-        const btn2 = document.querySelector("#btn2")
-        const btn3 = document.querySelector("#btn3")
-        const btn4 = document.querySelector("#btn4")
+            const botones = contendorBtn.querySelectorAll("button");
+            botones.forEach((button, index) => {
+                button.textContent = respuestas[index];
+                
+                if (respuestas[index] === correct_answer) {
+                    button.dataset.correcta = true;
+                } else {
+                    button.removeAttribute('data-correcta');
+                }
+            });
 
-    } else if (tipoPregunta == "boolean") {
-        const contendorBtn = document.createElement("div")
-        contendorBtn.id = "respuestas"
-        contendorBtn.innerHTML = `<button id="btn1">True</button>
+            botones.forEach(button => {
+                button.addEventListener('click', function () {
+                    if (this.dataset.correcta) {
+                        aciertos++
+                        contador++
+                        modalGood.showModal()
+
+                        setTimeout(() => {
+                            pregunta.innerHTML = ``
+                            contendorBtn.innerHTML = ``
+
+                            contAnsowers.appendChild(contendorBtn)
+                            const removecontendorBtn = document.querySelector("#respuestas")
+                            removecontendorBtn.remove()
+                            modalGood.close()
+
+                            imprimir()
+                            return
+                        }, 2000);
+
+                    } else {
+                        contador++
+
+                        modalBad.showModal()
+
+                        setTimeout(() => {
+                            pregunta.innerHTML = ``
+                            contendorBtn.innerHTML = ``
+
+                            contAnsowers.appendChild(contendorBtn)
+                            const removecontendorBtn = document.querySelector("#respuestas")
+                            removecontendorBtn.remove()
+                            modalBad.close()
+
+                            imprimir()
+                            return
+                        }, 3000);
+
+                    }
+                });
+            });
+
+
+            console.log(correct_answer);
+
+        } else if (tipoPregunta == "boolean") {
+            const contendorBtn = document.createElement("div")
+            contendorBtn.id = "respuestas"
+            contendorBtn.innerHTML = `<button id="btn1">True</button>
                     <button id="btn2">False</button>`
 
-        contAnsowers.appendChild(contendorBtn)
+            contAnsowers.appendChild(contendorBtn)
 
-        const btn1 = document.querySelector("#btn1")
-        const btn2 = document.querySelector("#btn2")
+            const btn1 = document.querySelector("#btn1")
+            const btn2 = document.querySelector("#btn2")
 
-
+        }
     }
 
+
+
 }
 
-function nextquestion() {
-   for (let i = 1; i < 10; i++) {
-        console.log(i);
-    } 
-}
