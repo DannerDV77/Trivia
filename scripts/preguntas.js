577fbbdb5f865a5726f7526a9f837bea6d7aacc4
hector castro <raidrex@hotmail.com>
2024-06-10 22:10:38 -0600

    preguntas random

--- scripts/preguntas.js
+++ scripts/preguntas.js
@@ -24,14 +24,27 @@ export function imprimir() {
         console.log("son preguntas multiples");
         const contendorBtn = document.createElement("div")
         contendorBtn.id = "respuestas"
-        contendorBtn.innerHTML = `<button id="btn1"></button>
-                    <button id="btn2"></button>
-                    <button id="btn3"></button>
-                    <button id="btn4"></button>`
+        contendorBtn.innerHTML = `
+        <button id="btn1"></button>
+        <button id="btn2"></button>
+        <button id="btn3"></button>
+        <button id="btn4"></button>`
 
         contAnsowers.appendChild(contendorBtn)
 
-        const btn1 = document.querySelector("#btn1")
+        const respuestas = [
+            correct_answer,
+            ...incorrect_answers
+        ].sort(() => Math.random() - 0.5)
+
+        const botones = contendorBtn.querySelectorAll("button")
+        botones.forEach((button, Index) => {
+            button.textContent = respuestas[Index]
+        })
+
+        console.log(respuestas, botones);
+
+        /* const btn1 = document.querySelector("#btn1")
         const btn2 = document.querySelector("#btn2")
         const btn3 = document.querySelector("#btn3")
         const btn4 = document.querySelector("#btn4")
@@ -39,10 +52,10 @@ export function imprimir() {
         btn1.innerHTML = `${correct_answer}`
         btn2.innerHTML = `${incorrect_answers[0]}`
         btn3.innerHTML = `${incorrect_answers[1]}`
-        btn4.innerHTML = `${incorrect_answers[2]}`
+        btn4.innerHTML = `${incorrect_answers[2]}` */
 
 
-    }else if (tipoPregunta == "boolean"){
+    } else if (tipoPregunta == "boolean") {
         console.log("son preguntas Boolean");
         const contendorBtn = document.createElement("div")
         contendorBtn.id = "respuestas"
@@ -54,7 +67,7 @@ export function imprimir() {
         const btn1 = document.querySelector("#btn1")
         const btn2 = document.querySelector("#btn2")
 
-        
+
     }
 
 }
